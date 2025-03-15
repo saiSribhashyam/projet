739b9c76e11c37dc18088f9e1a339aabfeb5c400
saiSribhashyam <110450908+saiSribhashyam@users.noreply.github.com>
2025-03-15 19:43:33 +0530

    added the pollCommits functions in the project mutation

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { pollCommits } from "@/lib/github";
 
 export const projectRouter= createTRPCRouter({
     createProject: protectedProcedure.input(
@@ -24,6 +25,7 @@ export const projectRouter= createTRPCRouter({
             }
         }
         })
+        await pollCommits(project.id)
         return project;
     }),
     getProjects: protectedProcedure.query(async ({ctx})=>{
@@ -35,6 +37,16 @@ export const projectRouter= createTRPCRouter({
                     }
                 },
                 deletedAt: null,
+            }
+        })
+    }),
+    getCommits: protectedProcedure.input(z.object({
+        projectId: z.string()
+    })).query(async ({ctx,input})=>{
+        pollCommits(input.projectId).then().catch(console.error)
+        return await ctx.db.commit.findMany({
+            where:{
+                projectId: input.projectId
             }
         })
     })
