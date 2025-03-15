346994923537d127a293bf8c6a41ad4164a5031e
saiSribhashyam <110450908+saiSribhashyam@users.noreply.github.com>
2025-03-16 01:18:30 +0530

    added gitHub document and embedding processing

---
+++ src/lib/github-loader.ts
@@ -0,0 +1,62 @@
+import {GithubRepoLoader} from '@langchain/community/document_loaders/web/github'
+import {Document} from '@langchain/core/documents'
+import { generateEmbedding, summariseCode } from './gemini'
+import { all } from 'axios'
+import { db } from '@/server/db'
+
+export const  loadGithubRepo = async (githubUrl: string, githubToken?: string)=>{
+    const loader = new GithubRepoLoader(githubUrl,{
+        accessToken: githubToken || '',
+        branch:'master',
+        ignoreFiles:['package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb'],
+        recursive: true,
+        unknown: 'warn',
+        maxConcurrency: 5,
+    })
+
+    const docs = await loader.load()
+    return docs
+}
+
+//console.log(await loadGithubRepo("https://github.com/saiSribhashyam/smartCalc-backend"))
+
+export const indexGithubRepo= async (projectId : string,githubUrl: string, githubToken?: string)=>{
+    const docs = await loadGithubRepo(githubUrl,githubToken)
+    const allEmbeddings= await generateEmbeddings(docs)
+
+    await Promise.allSettled(allEmbeddings.map(async (embedding,index)=>{
+        console.log(`processing ${index} of ${allEmbeddings.length}`)
+        if(!embedding) return 
+
+        const sourceCodeEmbedding = await db.sourceCodeEmbedding.create({
+            data: {
+                summary : embedding.summary,
+                sourceCode: embedding.sourceCode,
+                fileName: embedding.fileName,
+                projectId,
+
+
+            }
+        })
+
+        await db.$executeRaw`
+        UPDATE "SourceCodeEmbedding"
+        set "summaryEmbedding"= ${embedding.embedding}:: vector
+        where "id"= ${sourceCodeEmbedding.id}
+        `
+    }))
+}
+
+const generateEmbeddings = async (docs: Document[])=>{
+    return await Promise.all(docs.map(async doc=>{
+        const summary = await summariseCode(doc)
+        const embedding= await generateEmbedding(summary)
+        return {
+            summary,
+            embedding,
+            sourceCode: JSON.parse(JSON.stringify(doc.pageContent)),
+            fileName: doc.metadata.source,
+
+        }
+    }))
+}
